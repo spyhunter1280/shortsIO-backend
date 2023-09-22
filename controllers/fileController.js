89003510cd35b10956fb6fc2c8ee92965daeb5f1
spyhunter1280 <devhunter128@gmail.com>
2023-09-22 13:13:35 +0000

    update: third step test

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,33 +2,16 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const axios = require('axios');
+const path = require('path');
 
 dotenv.config();
-
-// URL of the file to transcribe
-const FILE_URL = "https://upcdn.io/W142iEb/raw/uploads/2023/09/17/4mADvc9q7P-BEN22-Telecommuting.mp3";
-
-// Request parameters
-const data = {
-    audio_url: FILE_URL // You can also use a URL to an audio or video file on the web
-};
-
 // HTTP request headers
 const headers = {
     "Authorization": process.env.YOUR_API_TOKEN,
     "Content-Type": "application/json"
 };
 
-// Submit for transcription via HTTP request
-async function submitTranscription() {
-    const response = await axios.post(process.env.transcript_endpoint, data, { headers: headers });
-
-    // Start polling for transcription completion
-    const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${response.data.id}`;
-    pollTranscription(pollingEndpoint);
-}
-
-async function exportSubtitles(api_token, transcriptId, format) {
+async function exportSubtitles(api_token, transcriptId, format, fileName) {
     const exportUrl = `https://api.assemblyai.com/v2/transcript/${transcriptId}/${format}`;
 
     const exportResponse = await axios.get(exportUrl, {
@@ -37,23 +20,33 @@ async function exportSubtitles(api_token, transcriptId, format) {
             'Authorization': api_token,
         }
     });
-    fs.writeFile('script.srt', exportResponse.data, function (err) {
+    fs.writeFile(`upload/${fileName}.srt`, exportResponse.data, function (err) {
         if (err) throw err;
-        console.log('File saved!');
+        formDataUpload({
+            accountId: process.env.accountId,
+            apiKey: process.env.apiKey,
+            filePath: `/home/ubuntu/shortsIO-backend/upload/${fileName}.srt`,
+            originalFileName: `${fileName}.srt`,
+        }).then(
+            response => {
+                console.log(`Success: ${JSON.stringify(response)}`);
+                res.status(400).json({ message: 'success', fileURL: response.fileUrl });
+            },
+            error => console.error(error)
+        );
     });
-
-    return true;
 }
 // Polling for transcription completion
-async function pollTranscription(pollingEndpoint) {
+async function pollTranscription(pollingEndpoint, fileName) {
     while (true) {
         const pollingResponse = await axios.get(pollingEndpoint, { headers: headers });
         const transcriptionResult = pollingResponse.data;
 
         if (transcriptionResult.status === 'completed') {
             // Print the results
-            const subtitles = await exportSubtitles(process.env.YOUR_API_TOKEN, transcriptionResult.id, 'srt');
+            const subtitles = await exportSubtitles(process.env.YOUR_API_TOKEN, transcriptionResult.id, 'srt', fileName);
             console.log('Subtitles:', subtitles);
+            return subtitles
             break;
         } else if (transcriptionResult.status === 'error') {
             throw new Error(`Transcription failed: ${transcriptionResult.error}`);
@@ -63,47 +56,48 @@ async function pollTranscription(pollingEndpoint) {
     }
 }
 
-async function uploadFromUrl(params) {
-    const baseUrl = "https://api.bytescale.com";
-    const path = `/v2/accounts/${params.accountId}/uploads/url`;
-
-    const response = await axios.post(`${baseUrl}${path}`, params.requestBody, {
+async function formDataUpload(params) {
+    const baseUrl = 'https://api.bytescale.com';
+    const path = `/v2/accounts/${params.accountId}/uploads/form_data`;
+    const entries = obj =>
+        Object.entries(obj).filter(([, val]) => val !== null && val !== undefined);
+    const query = entries(params.querystring ?? {})
+        .flatMap(([k, v]) => (Array.isArray(v) ? v.map(v2 => [k, v2]) : [[k, v]]))
+        .map(kv => kv.join('='))
+        .join('&');
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(params.filePath), params.originalFileName);
+    const response = await axios.post(`${baseUrl}${path}${query.length > 0 ? '?' : ''}${query}`, formData, {
         headers: {
-            "Authorization": `Bearer ${params.apiKey}`,
-            "Content-Type": "application/json",
-        }
+            Authorization: `Bearer ${params.apiKey}`,
+            'X-Upload-Metadata': JSON.stringify(params.metadata),
+            ...formData.getHeaders(),
+        },
     });
     const result = response.data;
-    if (response.status !== 200)
+    if (Math.floor(response.status / 100) !== 2)
         throw new Error(`Bytescale API Error: ${JSON.stringify(result)}`);
-
     return result;
-}
-
-exports.test = async (req, res) => {
-    res.status(400).json({ message: 'backend is working well' });
 }
 
 exports.uploadYoutube = async (req, res) => {
     const videoUrl = 'https://www.youtube.com/watch?v=G33j5Qi4rE8';
-    const outputFilePath = 'file.mp4';
+    const videoId = url.parse(youtubeUrl, true).query.v;
     ytdl(videoUrl)
-        .pipe(fs.createWriteStream(outputFilePath))
+        .pipe(fs.createWriteStream(`upload/${videoId}.mp4`))
         .on('finish', () => {
-
-            uploadFromUrl({
+            formDataUpload({
                 accountId: process.env.accountId,
                 apiKey: process.env.apiKey,
-                requestBody: {
-                    url: "https://rr4---sn-i3b7knse.googlevideo.com/videoplayback?expire=1694895823&ei=b7oFZbWgNp2h7OsPpMyEsAE&ip=166.88.141.98&id=o-AIzZUmfNqNx78lcbiizs913FODM3d5yM6Nh8sQnhYPa7&itag=299&aitags=133%2C134%2C135%2C136%2C160%2C242%2C243%2C244%2C247%2C278%2C298%2C299%2C302%2C303%2C394%2C395%2C396%2C397%2C398%2C399&source=youtube&requiressl=yes&mh=Fs&mm=31%2C29&mn=sn-i3b7knse%2Csn-i3belnlz&ms=au%2Crdu&mv=m&mvi=4&pl=24&initcwndbps=3338750&spc=UWF9f5E98yUGEuvfcpHzhNUXwa4TR8UmAmp7Y3XRzg&vprv=1&svpuc=1&mime=video%2Fmp4&ns=dwSkR5nEe9kxC5Sm7veXaKAP&gir=yes&clen=10159615&dur=366.400&lmt=1608545998353954&mt=1694873887&fvip=1&keepalive=yes&fexp=24007246&c=WEB&txp=5432432&n=ZDWYBty7T5aieQ&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAKjAHM6gibHRgwjKkOAlm2qsQHd6wuzAJunFyifB65nkAiEAzKD8S-XJukiA-lAS-U3nHp2fp9R2PhyC28z2RK4D1I4%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAJCj3mWFG8tG6T1E1iXm0HFVUi4vraxwpMYu3ILHKEdPAiAzg4vUez358phNSrBUmslz3OCNlvwEKQm7YmKaAmHuKw%3D%3D"
-                }
-            })
-                .then(response => {
+                filePath: `/home/ubuntu/shortsIO-backend/${videoId}.mp4`,
+                originalFileName: `${videoId}.mp4`,
+            }).then(
+                response => {
                     console.log(`Success: ${JSON.stringify(response)}`);
                     res.status(400).json({ message: 'success', fileURL: response.fileUrl });
-                })
-                .catch(error => console.error(error));
-            console.log('Video downloaded successfully!');
+                },
+                error => console.error(error)
+            );
         })
         .on('error', (error) => {
             resolveSoa.status(403).json({ message: error })
@@ -112,6 +106,17 @@ exports.uploadYoutube = async (req, res) => {
 }
 
 exports.generateSRT = async (req, res) => {
-    submitTranscription().catch((err) => console.error('Error:', err));
+    try {
+        const fileURL = req.fileURL;
+        const fileName = path.basename(fileURL);
+        const data = { audio_url: fileURL };
+        const response = await axios.post(process.env.transcript_endpoint, data, { headers: headers });
+        // Start polling for transcription completion
+        const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${response.data.id}`;
+        const result = pollTranscription(pollingEndpoint, fileName);
+        res.status(400).send({ message: result });
+    } catch (error) {
+        res.status(403).send({ message: 'error' });
+        console.log(error);
+    }
 }
-
